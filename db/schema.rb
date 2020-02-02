abb3cc01f34f01396a037a3f63d4dec79fa13790
rayyyy <nraavy@gmail.com>
2020-02-03 00:06:06 +0900

    format

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,27 +12,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_152744) do
-
+ActiveRecord::Schema.define(version: 20_200_127_152_744) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "keyboards", force: :cascade do |t|
-    t.string "name"
-    t.string "name_ja"
-    t.string "asin"
-    t.text "detail"
-    t.integer "price"
-    t.string "model_number"
-    t.float "keystroke"
-    t.date "date_first_available"
-    t.float "item_weight"
-    t.float "height"
-    t.float "width"
-    t.string "brand"
-    t.integer "matrix"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'keyboards', force: :cascade do |t|
+    t.string 'name'
+    t.string 'name_ja'
+    t.string 'asin'
+    t.text 'detail'
+    t.integer 'price'
+    t.string 'model_number'
+    t.float 'keystroke'
+    t.date 'date_first_available'
+    t.float 'item_weight'
+    t.float 'height'
+    t.float 'width'
+    t.string 'brand'
+    t.integer 'matrix'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
-
 end
